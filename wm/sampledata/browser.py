9d9c4851d999a742d2d42fae4f368459ad1e94a7
frisi <harald@webmeisterei.com>
2011-09-27 14:53:32 +0000

    return values for plugins (useful for unittests)

--- wm/sampledata/browser.py
+++ wm/sampledata/browser.py
@@ -23,9 +23,10 @@ class SampleDataView(BrowserView):
         show a status message that tells the user if the plugin could not be found,
         raised an error or ran successfully.
         """
+        result = None
         try:
             plugin = getUtility(ISampleDataPlugin,name=plugin)
-            plugin.generate(self.context)
+            result = plugin.generate(self.context)
             IStatusMessage(self.request).addStatusMessage(u"successfully ran %s" % plugin.title , 'info')
         except ComponentLookupError, e:
             IStatusMessage(self.request).addStatusMessage(u"could not find plugin %s" % plugin , 'error')
@@ -39,6 +40,7 @@ class SampleDataView(BrowserView):
         finally:
             #return to listing
             self.request.response.redirect(self.context.absolute_url() + '/@@'  + self.__name__)
+            return result
 
 
 
